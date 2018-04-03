fe8a0e0668bc6c325fbf8a5658d59647c3557ee3
Christopher David Desjardins <cddesjardins@gmail.com>
2018-04-03 11:01:26 -0500

    chapter 12 linted!

--- ch12.R
+++ ch12.R
@@ -1,10 +1,11 @@
-## ----eval = F------------------------------------------------------------
-## install.packages("ctv")
-## library("ctv")
+## -------------------------------------------------------------------
+## Chapter 12 R code
+## Handbook of Educational Measurement and Psychometrics Using R
+## C. D. Desjardins & O. Bulut
+## -------------------------------------------------------------------
 
-## ----eval = F------------------------------------------------------------
-## install.views("Psychometrics")
-
-## ----eval = F------------------------------------------------------------
-## update.views("Psychometrics")
-
+# cran task views ----
+install.packages("ctv")
+library("ctv")
+install.views("Psychometrics")
+update.views("Psychometrics")
